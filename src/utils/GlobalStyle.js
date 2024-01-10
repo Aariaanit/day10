fb24d93271dd79f6c82aa27e8e20dcd420e0e4e6
Aariaanit <arianittershnjaku@gmail.com>
2024-01-10 11:38:10 +0100

    10.01.2023

---
+++ src/utils/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    CustomFont: {
+        fontFamily: 'arial'
+    },
+    ButtonText: {
+        fontFamily: 'arial',
+        fontSize: 30,
+        padding: 10,
+    }
+});
